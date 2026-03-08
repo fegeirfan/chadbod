2ef28ef3c87fc0fa1ed3fa3ce68416d533056503
irfan117 <irfanasome@gmail.com>
2026-03-08 10:27:38 +0700

    dd

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import ChatHeader from "./components/chatbot/ChatHeader";
-import ChatMessages from "./components/chatbot/ChatMessages";
-import SuggestionButtons from "./components/chatbot/SuggestionButtons";
-import ChatInput from "./components/chatbot/ChatInput";
+import SapaChatApp from "./components/sapa/SapaChatApp";
 
 export default function Home() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-100 font-sans">
-      <div className="h-full w-full flex flex-col bg-white shadow-2xl">
-        <ChatHeader />
-        <ChatMessages />
-        <SuggestionButtons />
-        <ChatInput />
-      </div>
-    </div>
-  );
+  return <SapaChatApp />;
 }
